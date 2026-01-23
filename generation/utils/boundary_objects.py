59d78baac10eb92ea1a4654d65d233aff9f476c7
Pedro Basso <pedrobasso98@gmail.com>
2026-01-23 16:50:26 +0100

    addign code beauty on folder generation/utils

--- generation/utils/boundary_objects.py
+++ generation/utils/boundary_objects.py
@@ -4,6 +4,23 @@ from typing import List
 
 @dataclass
 class Province:
+    """
+    Represents a province within an autonomous region.
+
+    Attributes:
+        id (int): Unique identifier of the province.
+        code_province (str): Official province code.
+        name (str): Official name of the province.
+        alternative_name (str): Alternative or historical name of the province.
+        autonomic_region_code (str): Code of the autonomous region this province belongs to.
+        autonomic_region_name (str): Name of the autonomous region.
+        total_seats (int): Total number of seats assigned to the province.
+        latitude (float): Geographic latitude of the province.
+        longitude (float): Geographic longitude of the province.
+        population (int): Total population of the province.
+        iso_3166_2_code (str): ISO 3166-2 code of the province.
+    """
+
     id: int
     code_province: str
     name: str
@@ -19,6 +36,19 @@ class Province:
 
 @dataclass
 class AutonomousRegion:
+    """
+    Represents an autonomous region composed of multiple provinces.
+
+    Attributes:
+        code (str): Identifier code of the autonomous region.
+        name (str): Official name of the autonomous region.
+        alternative_name (str): Alternative or localized name.
+        senators (int): Number of senators assigned to the region.
+        total_seats (int): Total number of seats assigned to the region.
+        iso_3166_2_code (str): ISO 3166-2 code of the autonomous region.
+        provinces (List[Province]): Provinces belonging to this autonomous region.
+    """
+
     code: str
     name: str
     alternative_name: str
@@ -30,15 +60,37 @@ class AutonomousRegion:
 
 @dataclass
 class Country:
+    """
+    Represents a country composed of autonomous regions.
+
+    Attributes:
+        regions (List[AutonomousRegion]): Autonomous regions within the country.
+    """
+
     regions: List[AutonomousRegion]
 
 
 @dataclass
 class PoliticalParty:
+    """
+    Represents a political party.
+
+    Attributes:
+        name (str): Name of the political party.
+        popularity (float): Popularity score of the party (e.g., percentage of support).
+    """
+
     name: str
     popularity: float
 
 
 @dataclass
 class Parties:
+    """
+    Container for political parties.
+
+    Attributes:
+        parties (List[PoliticalParty]): List of political parties.
+    """
+
     parties: List[PoliticalParty]
--- generation/utils/logging_config.py
+++ generation/utils/logging_config.py
@@ -8,6 +8,7 @@ LOG_DIR.mkdir(exist_ok=True)
 
 LOG_FILE = LOG_DIR / "votes_generator.log"
 
+
 def setup_logging(level=logging.INFO):
     formatter = logging.Formatter(
         "%(asctime)s | %(levelname)-8s | %(name)s | %(message)s"
@@ -16,13 +17,9 @@ def setup_logging(level=logging.INFO):
     console_handler = logging.StreamHandler(sys.stdout)
     console_handler.setFormatter(formatter)
 
-    file_handler = RotatingFileHandler(
-        LOG_FILE, maxBytes=5_000_000, backupCount=5
-    )
+    file_handler = RotatingFileHandler(LOG_FILE, maxBytes=5_000_000, backupCount=5)
     file_handler.setFormatter(formatter)
 
     logging.basicConfig(
-        level=level,
-        handlers=[console_handler, file_handler],
-        force=True
+        level=level, handlers=[console_handler, file_handler], force=True
     )
